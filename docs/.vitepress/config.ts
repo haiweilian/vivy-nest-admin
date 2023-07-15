d044479896726e15c01f6bd24025f9ec6b1bb842
haiweilian <haiweilian@foxmail.com>
2023-07-15 19:17:48 +0800

    docs: init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'vivy-nest-admin',
+  description: '基于 Nest.js & React.js 的后台权限管理系统',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '后端', link: '/back/introduce' },
+      { text: '前端', link: '/front/introduce' },
+    ],
+    sidebar: [
+      {
+        text: '介绍',
+        link: '/introduce',
+      },
+      {
+        text: '后端',
+        items: [{ text: '介绍', link: '/back/introduce' }],
+      },
+      {
+        text: '前端',
+        items: [{ text: '介绍', link: '/front/introduce' }],
+      },
+    ],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/haiweilian/vivy-nest-admin' }],
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,20 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: vivy-nest-admin
+  tagline: 基于 Nest.js & React.js 的后台权限管理系统
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /introduce
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
