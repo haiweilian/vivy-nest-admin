32b27c5d6576f65a761c83dc03579b23614fc614
haiweilian <haiweilian@foxmail.com>
2025-12-17 19:52:48 +0800

    chore: rename types

--- vivy-react/types/request.d.ts
+++ vivy-react/types/request.d.ts
@@ -76,3 +76,17 @@ declare interface PaginateParams {
    */
   readonly limit?: number
 }
+
+/**
+ * 响应对象
+ */
+declare interface AjaxResult<T = any> {
+  /** 状态码 */
+  code: number
+
+  /** 数据 */
+  data?: T
+
+  /** 消息 */
+  message?: string
+}
